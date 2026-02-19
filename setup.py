7a0bc41fbaeb37a493afe89125a76820f7a0c000
rakeshsuzan-gif <rakeshsuzan@gmail.com>
2026-02-19 12:32:33 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function  will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_object:
+        requirements=file_object.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+
+    return requirements
+setup(
+name='churn prediction',
+version='0.0.1',
+author='rakesh',
+author_email='suzanrakesh@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
